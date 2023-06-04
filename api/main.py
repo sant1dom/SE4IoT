c0a80dbea52d75c363468bfb83c440a1662523b8
lucadisab <ds97luca@gmail.com>
2023-06-04 16:56:11 +0200

    First commit

--- api/main.py
+++ api/main.py
@@ -5,7 +5,7 @@ import paho.mqtt.client as mqtt
 
 app = FastAPI()
 
-origins = ["http://localhost", "http://localhost:8080", "http://localhost:8081"]
+origins = ["http://localhost", "http://localhost:8080", "http://localhost:8081", "http://localhost:3000", "http://localhost:3001"]
 
 app.add_middleware(
     CORSMiddleware,
